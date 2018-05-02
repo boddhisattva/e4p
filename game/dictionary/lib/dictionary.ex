f95fe5d0d4f88ca0118a1e4c74f62906dbe9e5e2
Mohnish G J <mail4mohnish@gmail.com>
2018-05-02 17:54:50 +0530

    Add the ability to get a random word from a text file that serves as a dictionary

--- game/dictionary/lib/dictionary.ex
+++ game/dictionary/lib/dictionary.ex
@@ -12,7 +12,13 @@ defmodule Dictionary do
       :world
 
   """
-  def hello do
-    :world
+
+  def random_word do
+    Enum.random(word_list())
+  end
+
+  def word_list do
+    contents = File.read!("assets/words.txt")
+    words = String.split(contents, ~r/\n/)
   end
 end
