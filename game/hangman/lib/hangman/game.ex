fc97be7c07678693b84e53f6e559eca01042a406
Mohnish G J <mail4mohnish@gmail.com>
2018-05-06 09:58:55 +0530

    Remove new_game code related duplication through defdelegate
    There is no point in having the new_game related code in both
    hangman.ex and game.ex, hence delegating the code to game.ex. This way
    we have lesser duplication of code among both modules.

--- game/hangman/lib/hangman/game.ex
+++ game/hangman/lib/hangman/game.ex
@@ -1,4 +1,15 @@
 defmodule Hangman.Game do
+  @moduledoc """
+  Documentation for Hangman.
+  """
+
+  @doc """
+  Starting a new Hangman game
+  ## Examples
+      iex> Hangman.new_game
+      Hello from the Hangman Game
+  """
+
   def new_game do
     IO.puts "Hello from the Hangman Game"
   end
