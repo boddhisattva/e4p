2c2e0515430720d389862f8ddb93b31a93cd51e9
Mohnish G J <mail4mohnish@gmail.com>
2018-05-06 16:51:40 +0530

    Add the make move related version for when one loses a game

--- game/hangman/test/hangman/game_test.exs
+++ game/hangman/test/hangman/game_test.exs
@@ -17,4 +17,10 @@ defmodule GameTest do
            |> Map.put(:game_state, :won)
     assert { ^game, _ } = Game.make_move(game, "x")
   end
+
+  test "state isn't changed for :lost game" do
+    game = Game.new_game()
+           |> Map.put(:game_state, :lost)
+    assert { ^game, _ } = Game.make_move(game, "x")
+  end
 end
