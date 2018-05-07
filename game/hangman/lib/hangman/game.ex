ac7b41adfe8d018b61465b1cdf0118361d1ba9b3
Mohnish G J <mail4mohnish@gmail.com>
2018-05-07 18:48:11 +0530

    Add the ability to check for duplicate moves
    - Need to account for letters that have been guessed
    - To check if a letter is picked or not picked, the ideal data
    structure for that is a Set.

--- game/hangman/lib/hangman/game.ex
+++ game/hangman/lib/hangman/game.ex
@@ -6,7 +6,8 @@ defmodule Hangman.Game do
   defstruct(
     turns_left: 7,
     game_state: "initializing",
-    letters: []
+    letters: [],
+    used:    MapSet.new()
     )
 
   @doc """
@@ -31,7 +32,49 @@ defmodule Hangman.Game do
     { game, tally(game) }
   end
 
+  def make_move(game, guessed_letter) do
+    require IEx; IEx.pry
+    game = accept_move(game, guessed_letter, MapSet.member?(game.used, guessed_letter))
+    { game, tally(game) }
+  end
+
+  @doc """
+  If a letter is already used, we are gonna return a game with the game state of
+  already used..
+  ## Examples
+  iex(3)> Hangman.Game.accept_move(game, guessed_letter, true)
+%Hangman.Game{
+  game_state: :already_used,
+  letters: ["g", "e", "o", "r", "g", "i", "a"],
+  turns_left: 7,
+  used: #MapSet<[]>
+}
+  """
+  def accept_move(game, guessed_letter, _already_guessed = true) do
+    Map.put(game, :game_state, :already_used)
+  end
+
+  @doc """
+  If not used, we are going to update the list of letters saying that
+  we have now used this letter..
+  ## Examples
+iex(4)> Hangman.Game.accept_move(game, guessed_letter, false)
+%Hangman.Game{
+  game_state: "initializing",
+  letters: ["g", "e", "o", "r", "g", "i", "a"],
+  turns_left: 7,
+  used: #MapSet<["x"]>
+}
+
+  """
+  def accept_move(game, guessed_letter, _already_guessed) do
+    Map.put(game, :used, MapSet.put(game.used, guessed_letter))
+  end
+
+
   def tally(game) do
     123
   end
 end
+
+
