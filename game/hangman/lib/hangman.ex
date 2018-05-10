53133ec481e049ccb7f6fad0d244a84a95ba541c
Mohnish G J <mail4mohnish@gmail.com>
2018-05-10 09:53:07 +0530

    Fix argument error related bug which prevented the game from executing
    Fix Argument error related bug:
    
    ** (ArgumentError) argument error
        :erlang.apply(%Hangman.Game{game_state: "initializing", letters:
    ["y", "o", "u", "t", "h"], turns_left: 7, used: #MapSet<[]>},
    :make_move, [])

--- game/hangman/lib/hangman.ex
+++ game/hangman/lib/hangman.ex
@@ -15,7 +15,7 @@ defmodule Hangman do
   defdelegate tally(game),            to: Game
 
   def make_move(game, guess) do
-    game = game.make_move(game, guess)
+    game = Game.make_move(game, guess)
     {game, tally(game)}
   end
 end
