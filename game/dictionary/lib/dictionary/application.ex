1b7ab48436db1a850f42e87ae3de7e480ff1fb38
Mohnish G J <mail4mohnish@gmail.com>
2018-05-12 12:56:23 +0530

    Implement an initial version of the Dictionary with Supervisors

--- game/dictionary/lib/dictionary/application.ex
+++ game/dictionary/lib/dictionary/application.ex
@@ -5,6 +5,26 @@ defmodule Dictionary.Application do
   """
 
   def start(_type, _args) do
-    Dictionary.WordList.start_link()
+    # Inorder to use the worker; we need to import them from a supervisor library.
+    import Supervisor.Spec
+
+    children = [
+      worker(Dictionary.WordList, [])
+    ]
+    # Our children are gonna be a list of worker processes
+    # worker() is a helper that is used to specify it..
+
+    options = [
+      name: Dictionary.Supervisor, #gives the supervisor a name
+      strategy: :one_for_one # how to restart the processes;
+    ] # what it does is; each if the processes it monitors are independent ; if one dies; they only of them
+    # then we restart only that one particular process..
+    # strategy is what is used to restart the processes;
+
+    # children are worker processes; we use a helper to specify it
+
+    Supervisor.start_link(children, options)
+    # this would start the child processes and monitor them
+    # according to the options we setup..;
   end
 end
