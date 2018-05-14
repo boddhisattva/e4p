baa9829e84ac13497073007a415fa082e5835bad
Mohnish G J <mail4mohnish@gmail.com>
2018-05-14 15:44:04 +0530

    Add more Ebert fixes

--- game/hangman/lib/hangman/server.ex
+++ game/hangman/lib/hangman/server.ex
@@ -13,7 +13,7 @@ defmodule Hangman.Server do
 
   def handle_call({:make_move, guess}, _from, game) do
     {game, tally} = Game.make_move(game, guess)
-    { :reply, tally, game }
+    {:reply, tally, game}
   end
 
   def handle_call({:tally}, _from, game) do
